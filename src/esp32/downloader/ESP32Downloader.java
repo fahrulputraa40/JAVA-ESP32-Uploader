5a0be081c544a049a678ff6c6a9bc5b7801be9e2
fahrulputraa40 <fahrulputraa40@gmail.com>
2021-03-24 20:53:39 +0700

    ok

---
+++ src/esp32/downloader/ESP32Downloader.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esp32.downloader;
+
+/**
+ *
+ * @author fahrul
+ */
+public class ESP32Downloader {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        App app = new App();
+        app.setTitle("ESP32 Uploader");
+        app.setVisible(true);
+        
+    }
+    
+}
